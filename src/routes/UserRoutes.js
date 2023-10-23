47c4a3201cfb47b7c6990b1ac15009c5fa6b3f72
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2023-10-23 16:04:36 -0300

    feat: added user routes

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { createNewUser } = require("../controller/UserController")
+const { createNewUser, listAllUsers, userUpdate, userDelete } = require("../controller/UserController")
 
-router.post("/create", createNewUser)
+router.post("/", createNewUser);
+router.get("/", listAllUsers);
+router.put("/:id", userUpdate);
+router.delete("/:id", userDelete);
 
 module.exports = router;
